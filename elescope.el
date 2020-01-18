cd89b94598d793692c121428701808e93ffd8af9
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-01-18 11:12:08 +0000

    stating your mission in the first step

---
+++ elescope.el
@@ -0,0 +1,13 @@
+;;; elescope --- Summary
+
+;;; Commentary:
+;;; Clone remote projects in a flash.
+
+;;; Code:
+(defcustom elescope-root-folder nil
+  "Directory to clone projects into."
+  :group 'elescope-variable
+  :type 'directory)
+
+(provide 'elescope)
+;;; elescope.el ends here
