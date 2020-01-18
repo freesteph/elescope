172b6dab9389411f7ec621e94917a81d32e5ecd3
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-01-18 18:58:03 +0000

    fix: show a status message whilst the request is firing

--- elescope.el
+++ elescope.el
@@ -13,31 +13,40 @@
 
 (defvar elescope-forges
   '(github gitlab)
-  "Forges understood by elescope")
+  "Forges understood by elescope.")
 
 (defun elescope--parse-gh (data)
-  "Parses the DATA returned by GitHub and maps a list of names."
+  "Parse the DATA returned by GitHub and maps a list of names."
   (mapcar
    (lambda (i) (alist-get 'name i))
    (seq-take (alist-get 'items data) 10)))
 
-(elescope--parse-gh peach-data)
+(defun elescope--call-gh (name)
+  "Search for GitHub repositories matching NAME."
+  (request
+       "https://api.github.com/search/repositories"
+       :params '(("q" . name))
+       :parser 'json-read
+       :success (cl-function
+                 (lambda (&key data &allow-other-keys)
+                   (let ((results (elescope--parse-gh data)))
+                     (ivy-update-candidates results))))))
 
 (defun elescope--cb (str)
+  "Handle the minibuffer STR query and search the relevant forge."
   (or
    (ivy-more-chars)
    (progn
      (message "firing request for %s" str)
-     (request
-       "https://api.github.com/search/repositories"
-       :params '(("q" . str))
-       :parser 'json-read
-       :success (cl-function
-                 (lambda (&key data &allow-other-keys)
-                   (ivy-update-candidates (elescope--parse-gh data)))))
-     0)))
+     (elescope--call-gh str)
+     (list "" (format "Looking for repositories matching %s..." str)))
+   0))
 
 (defun elescope-checkout (select-forges)
+  "Prompt a repository name to search for.
+
+If the function is called with the prefix SELECT-FORGES argument,
+prompt a forge to search from (defaults to GitHub)."
   (interactive "P")
   (if select-forges
       (completing-read "Forge: " elescope-forges)
