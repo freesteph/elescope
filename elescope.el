22cad5c6684474ae94c27bd288b833eaee1f7736
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-01-19 19:13:18 +0000

    docs: add a license

--- elescope.el
+++ elescope.el
@@ -1,5 +1,29 @@
 ;;; elescope --- Summary
 
+;; Copyright (C) 2020 Stéphane Maniaci
+
+;; Author: Stéphane Maniaci <stephane dot maniaci at gmail.com>
+;; URL: https://github.com/freesteph/elescope
+;; Package-Version: 20200117.410
+;; Version: 0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;; elescope.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; elescope.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with elescope.el.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+
 ;; -*- lexical-binding: t -*-
 
 ;;; Commentary:
